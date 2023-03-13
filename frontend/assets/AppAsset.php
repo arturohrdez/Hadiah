abce6c823de15706872db755331a653c77517468
Arturo Hernández <arturo@vicom.mx>
2023-03-13 11:20:05 -0600

    Frontend [New Assets]

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,23 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/vendor/animate.css/animate.min.css',
+        'css/vendor/bootstrap-icons/bootstrap-icons.css',
+        'css/vendor/boxicons/css/boxicons.min.css',
+        'css/vendor/glightbox/css/glightbox.min.css',
+        'css/vendor/remixicon/remixicon.css',
+        'css/vendor/remixicon/remixicon.css',
+        'css/vendor/swiper/swiper-bundle.min.css',
+        'css/style.css',
     ];
     public $js = [
+        'js/main.js',
+        'js/vendor/bootstrap.bundle.min.js',
+        'js/vendor/glightbox.min.js',
+        'js/vendor/isotope.pkgd.min.js',
+        'js/vendor/swiper-bundle.min.js',
+        'js/vendor/noframework.waypoints.js',
+        //'js/vendor/php-email-form/validate.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
