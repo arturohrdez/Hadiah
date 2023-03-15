d679c9e278ddde0bd7697d14e20ac62f3cf0daf0
Arturo Hernández <arturo@vicom.mx>
2023-03-14 18:50:08 -0600

    Frontend [Rifa Detail - Tickets]

--- frontend/views/site/rifaDetail.php
+++ frontend/views/site/rifaDetail.php
@@ -54,12 +54,25 @@ use yii\helpers\Html;
 						</div>
 					</div>
 
+					<div id="div_selected" class="row pt-3 pb-2 bg-black" style="display: none;">
+						<?php echo Html::hiddenInput('tn_sel', $value = "",['id'=>'tn_sel']); ?>
+						<div id="div_options" class="col-12 text-white text-center">
+							<p class="fs-3 text-warning">
+								<span class="n_t">0</span> - Boletos Seleccionados
+							</p>
+							<p class="t_opt"><button class='btn_ticketDel'></button></p>
+							<p class="fs-5 text-warning">
+								Para eliminar haz clic en el boleto.
+							</p>
+						</div>
+					</div>
+
 					<div class="row mt-5">
 							<?php 
 							$init = $model->ticket_init;
 							$end  = $model->ticket_end;
 							for ($i=$init; $i <= $end ; $i++) { 
-								echo '<div class="col-lg-1 col-sm-2 col-4">'.Html::button($tickets[$i], ['class' => 'btn btn-success mb-3']).'</div>';
+								echo '<div class="col-lg-1 col-sm-2 col-4">'.Html::button($tickets[$i], ['class' => 'btn_ticket btn btn-outline-success mb-3','data-tn'=>$tickets[$i]]).'</div>';
 							}
 							?>
 					</div>
@@ -68,8 +81,61 @@ use yii\helpers\Html;
 		</div>
 	</div>
 </section>
+
+
+
 <?php 
-/*echo "<pre>";
-var_dump($model->attributes);
-echo "</pre>";*/
-?>
+$script = <<< JS
+	$(function(e){
+		var tn_sel = $("#tn_sel").val("");
+	});
+
+
+
+
+	$(".btn_ticket").on("click",function(e){
+		var tn     = $(this).data("tn");
+		var tn_sel = $("#tn_sel").val();
+
+		let elements = [];
+		if(tn_sel.length > 0){
+			var exp  = tn_sel.split(',');
+			elements = exp;
+			elements.push(tn);
+			$("#tn_sel").val(elements.join(','));
+		}else{
+			elements.push(tn);
+			$("#tn_sel").val(elements.join(','));
+		}//end if
+
+		let n_t = elements.length;
+		$(".n_t").text(n_t);
+
+		let t_selectBtn = "";
+		for (var i = n_t-1; i >= 0; i--) {
+			//console.log(elements[i]);
+			t_selectBtn = t_selectBtn + "<button class='btn_ticketDel btn btn-danger ml-2' type='button' data-tn='"+elements[i]+"'>"+elements[i]+"</button>";
+		}//end foreach*
+
+		$(".t_opt").html(t_selectBtn);
+
+		//console.log(t_selectBtn)
+
+		$("#div_selected").show();
+		$(this).removeClass('btn-outline-success');
+		$(this).addClass('btn-success');
+	});
+
+	$(".btn_ticketDel").on("click",function(e){
+		alert("entra");
+		/*var tn     = $(this).data("tn");
+		var tn_sel = $("#tn_sel").val();
+
+		var exp  = tn_sel.split(',');
+		let pos_del = tn_sel.indexOf(tn) // (pos) es la posición para abreviar
+
+		console.log(exp)
+		console.log(pos_del)*/
+	});
+JS;
+$this->registerJs($script);
