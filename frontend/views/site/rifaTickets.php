39180c338389f202e4c2c11f7cab478170110ae3
Arturo Hernández <arturo@vicom.mx>
2023-04-10 18:40:25 -0600

    Frontend [Detalle del ticket - ajuste responsive]

--- frontend/views/site/rifaTickets.php
+++ frontend/views/site/rifaTickets.php
@@ -24,7 +24,7 @@
 			}else{//end if
 			?>
 			<div class="d-flex justify-content-center">
-				<div class="col-md-6 mt-md-0">
+				<div class="col-md-12 col-lg-7 mt-md-0">
 					<div class="box">
 						<h3>&nbsp;</h3>
 						<?php 
